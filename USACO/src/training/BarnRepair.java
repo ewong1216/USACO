9f1f9f445db3b78b7340ed92559d4008de86a005
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2018-10-25 21:12:59 -0700

    1.4 Barn Repair

--- USACO/src/training/BarnRepair.java
+++ USACO/src/training/BarnRepair.java
@@ -6,6 +6,7 @@ package training;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class BarnRepair{
@@ -18,8 +19,31 @@ public class BarnRepair{
 		firstline = firstline.substring(firstline.indexOf(" ")+1);
 		int numcows = Integer.parseInt(firstline);
 		int[] stalls = new int[numstalls];
+		int[] occupied = new int[numcows];
 		for(int i = 0; i < numcows; i++)
-			stalls[Integer.parseInt(scan.nextLine())] = 1;
-		//int minstalls = 
+			occupied[i] = Integer.parseInt(scan.nextLine());
+		for(int i = 0; i < numcows; i++)
+			stalls[occupied[i]] = 1;
+		int minstalls = occupied[occupied.length-1] - occupied[0] + 1;
+		
+		int[] spaces = new int[numcows/2];
+		int numspaces = 0;
+		for(int i = occupied[0] + 1; i < occupied[occupied.length-1]; i++){
+			int spacelength = 0;
+			while(stalls[i] == 0){
+				spacelength++;
+				i++;
+			}
+			if(spacelength > 0){
+				spaces[numspaces] = spacelength;
+				numspaces++;
+			}
+		}
+		
+		Arrays.sort(spaces);
+		for(int i = spaces.length-1; i > spaces.length - numboards; i--)
+			minstalls -= spaces[i];
+		
+		System.out.println(minstalls);
 	}
 }
