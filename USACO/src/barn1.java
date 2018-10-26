f8dfa84d134a48e2da315c70968408633ff5f5a0
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2018-10-25 21:46:04 -0700

    barn1 completed

--- USACO/src/barn1.java
+++ USACO/src/barn1.java
@@ -1,17 +1,24 @@
-package training;
 /*
- * 
+USER: ewong121
+TASK: barn1
+LANG: JAVA
  */
-//1.4
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class BarnRepair{
-	public static void main(String[] args) throws FileNotFoundException{
-		Scanner scan = new Scanner(new File("src/barn1.in"));
+public class barn1{
+	public static void main(String[] args) throws IOException{
+		//File f = new File("src/barn1.in");
+		//FileWriter fw = new FileWriter("src/barn1.out");
+		File f = new File("barn1.in");
+		FileWriter fw = new FileWriter("barn1.out");
+		Scanner scan = new Scanner(f);
+		BufferedWriter bw = new BufferedWriter(fw);
 		String firstline = scan.nextLine();
 		int numboards = Integer.parseInt(firstline.substring(0, firstline.indexOf(" ")));
 		firstline = firstline.substring(firstline.indexOf(" ")+1);
@@ -21,12 +28,24 @@ public class BarnRepair{
 		int[] stalls = new int[numstalls];
 		int[] occupied = new int[numcows];
 		for(int i = 0; i < numcows; i++)
-			occupied[i] = Integer.parseInt(scan.nextLine());
+			occupied[i] = Integer.parseInt(scan.nextLine()) - 1;
+		Arrays.sort(occupied);
+		int minstalls = occupied[occupied.length-1] - occupied[0] + 1;
+		if(numboards == 1){
+			bw.write(minstalls + "\n");
+			bw.close();
+			return;
+		}
+		if(numboards > numcows){
+			bw.write(numcows + "\n");
+			bw.close();
+			return;
+		}
 		for(int i = 0; i < numcows; i++)
 			stalls[occupied[i]] = 1;
-		int minstalls = occupied[occupied.length-1] - occupied[0] + 1;
 		
-		int[] spaces = new int[numcows/2];
+		
+		int[] spaces = new int[numcows - 1];
 		int numspaces = 0;
 		for(int i = occupied[0] + 1; i < occupied[occupied.length-1]; i++){
 			int spacelength = 0;
@@ -45,5 +64,7 @@ public class BarnRepair{
 			minstalls -= spaces[i];
 		
 		System.out.println(minstalls);
+		bw.write(minstalls+"\n");
+		bw.close();
 	}
 }
