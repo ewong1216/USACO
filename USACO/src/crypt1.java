ffb061d76da5cd9db7be6495e921d07661befb32
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2018-11-01 13:31:56 -0700

    getMultiples from list of digits

--- USACO/src/crypt1.java
+++ USACO/src/crypt1.java
@@ -27,12 +27,43 @@ public class crypt1{
 			numsline = numsline.substring(numsline.indexOf(" ") + 1);
 		}
 		nums[nums.length-1] = Integer.parseInt(numsline);
+		int nMults = numMultiples(nums);		
+		if(nMults == 0){
+			bw.write(0 + "\n");
+			bw.close();
+			return;
+		}
+		int[][] mults = getMultiples(nMults, nums);
+		
 		int numsolutions = 0;
 		
 		scan.close();
 		bw.close();
 	}
-	
+	public static int numMultiples(int[] nums){
+		int count = 0;
+		for(int i = 0; i < nums.length; i++){
+			for(int j = i; j < nums.length; j++){
+				if(arrcontains(nums[i] * nums[j], nums))
+					count++; 
+			}
+		}
+		return count;
+	}
+	public static int[][] getMultiples(int count, int[] nums){
+		int[][] mults = new int[count][2];
+		int track = 0;
+		for(int i = 0; i < nums.length; i++){
+			for(int j = i; j < nums.length; j++){
+				if(arrcontains(nums[i] * nums[j], nums)){
+					mults[track][0] = nums[i];
+					mults[track][1] = nums[j];
+					track++;
+				}
+			}
+		}
+		return mults;
+	}
 	public static boolean containsInts(String s, int[] nums){
 		for(int i = 0; i < s.length(); i++){
 			if(!arrcontains(Integer.parseInt(s.substring(i, i+1)),nums))
